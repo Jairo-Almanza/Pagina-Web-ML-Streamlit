0050f366ac8f8461419319ccbed2ca47f93866f8
Andrés Almanza Díaz <82005799+Jairo-Almanza@users.noreply.github.com>
2024-09-25 04:04:08 +0000

    Despliegue

--- src/app.py
+++ src/app.py
@@ -1,4 +1,20 @@
-from utils import db_connect
-engine = db_connect()
+import streamlit as st
+import pandas as pd
+from pickle import load
+import os
 
-# your code here
+prediction = 0
+
+model = load(open("../models/model_xgbregressor_42_studentperformance.sav", "rb"))
+
+st.title("Student Performance")
+
+val1 = st.slider("Attendance", min_value=0.0, max_value=100.0, step=0.1)
+val2 = st.slider("Hours studied", min_value=0.0, max_value=100.0, step=0.1)
+val3 = st.slider("Access to Resources", min_value=0.0, max_value=4.0, step=0.1)
+val4 = st.slider("Parental Involvement", min_value=0.0, max_value=4.0, step=0.1)
+
+if st.button("Predict"):
+    prediction = str(model.predict([[val1, val2, val3, val4]])[0])
+    pred_class = prediction
+    st.write("Prediction:", pred_class)
