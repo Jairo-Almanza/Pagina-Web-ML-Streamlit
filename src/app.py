1e2e67a005d9a5d46dd8cb3e420d5f03498d6ad5
Andrés Almanza Díaz <82005799+Jairo-Almanza@users.noreply.github.com>
2024-09-25 05:30:09 +0000

    despliegue

--- src/app.py
+++ src/app.py
@@ -9,10 +9,10 @@ model = load(open("../models/model_xgbregressor_42_studentperformance.sav", "rb"
 
 st.title("Student Performance")
 
-val1 = st.slider("Attendance", min_value=0.0, max_value=100.0, step=0.1)
-val2 = st.slider("Hours studied", min_value=0.0, max_value=100.0, step=0.1)
-val3 = st.slider("Access to Resources", min_value=0.0, max_value=4.0, step=0.1)
-val4 = st.slider("Parental Involvement", min_value=0.0, max_value=4.0, step=0.1)
+val1 = st.slider("Attendance", min_value=60.0, max_value=100.0, step=1)
+val2 = st.slider("Hours studied", min_value=1.0, max_value=60.0, step=1)
+val3 = st.slider("Access to Resources", min_value=0.0, max_value=2.0, step=1)
+val4 = st.slider("Parental Involvement", min_value=0.0, max_value=2.0, step=1)
 
 if st.button("Predict"):
     prediction = str(model.predict([[val1, val2, val3, val4]])[0])
